3265eeed87e6571f081b16ab4e36194948881e14
dankchan09 <mainhuy2406bp@gmail.com>
2025-02-13 00:14:19 +0700

    Xóa toàn bộ file cũ

--- game_hub/dash/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.models import User
-admin.site.site_header = 'Game Hub Administration'
-admin.site.site_title = 'Game Hub'
-admin.site.index_title = 'Welcome to Game Hub Admin Panel'
-
-admin.register(User)
-
-
--- game_hub/player/apps.py
+++
@@ -1,8 +0,0 @@
-# apps.py
-from django.apps import AppConfig
-
-class AccountsConfig(AppConfig):
-    name = 'player'  
-
-    def ready(self):
-        import player.signals  
